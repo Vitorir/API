d76ebe7226e794e4787511c69eabdd2bca377d28
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-04-10 13:23:45 -0300

    Initializes cep api

---
+++ api-cep/scripts.js
@@ -0,0 +1,7 @@
+// Pegando os campos dos formularios
+// utilizando queryselector
+const end = document.querySelector("#endereco");
+const bairro = document.querySelector("#bairro");
+const cidade = document.querySelector("#cidade");
+const estado = document.querySelector("#estado");
+const cep = document.querySelector("#cep");
